d48138eaa9053273bc014e1f51c088021b231d40
Evando Silva <evando_5@hotmail.com>
2023-09-07 16:21:06 -0300

    att

---
+++ curso_python/aula8.py
@@ -0,0 +1,12 @@
+nome = "Evando"
+sobreNome = "Silva"
+idade = 41
+data_nasc = 1982
+maior_idade = True
+altura = 1.83
+print('NOme: ',nome)
+print('Sobre Nome: ',sobreNome)
+print('Idade: ',idade)
+print('Ano de Nasc: ',data_nasc)
+print('É maior de idade: ',maior_idade)
+print('Altura: ',altura)
