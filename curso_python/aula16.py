df745164c5f47699f4ca67a00afce4677d40824b
Evando Silva <evando_5@hotmail.com>
2023-09-10 10:10:20 -0300

    att

---
+++ curso_python/aula16.py
@@ -0,0 +1,9 @@
+entrada = input("Voce entrou no sistema? ")
+
+if entrada == 'entrar':
+    print("Voce entrou no sistema")
+elif entrada == 'sair':
+     print("Voce saiu do sistema")
+
+else:
+    print("Vc não digitou nem entrar nem sair")       
---
+++ curso_python/aula17.py
@@ -0,0 +1,15 @@
+condicao1 = False
+condicao2 = True
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print('Codigo para condição 1')
+elif condicao2:
+    print('Codigo para condição 2')
+elif condicao3:
+    print('Codigo para condição 3') 
+elif condicao3:
+    print('Codigo para condição 4')  
+else:
+    print('Nenhuma condição foi satisfeito')
