afd7eb067205738b9eab0fb54ee0b4e9eea5c12e
Evando Silva <evando_5@hotmail.com>
2023-09-11 22:03:40 -0300

    att

---
+++ curso_python/aula18.py
@@ -0,0 +1,9 @@
+primeiro = input("Digite o primeiro Valor: ")
+segundo = input("Digite o segundo Valor: ")
+if primeiro >= segundo:
+    print("O Primeiro é maior que o segundo")
+else:
+    print("O segundo é maior que o primeiro")
+    
+    
+        
