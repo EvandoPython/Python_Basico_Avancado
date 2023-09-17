5fd9aa52c6ff3010a261f1dff166281efb7fe235
Evando Silva <evando_5@hotmail.com>
2023-09-17 09:51:49 -0300

    att

---
+++ curso_python/aula32.py
@@ -0,0 +1,11 @@
+
+condicao = False
+
+if condicao:
+    print('Faça açgo')
+else:
+    print('Não Faça açgo')
+        
+ 
+     
+    
---
+++ curso_python/aula33.py
@@ -0,0 +1,15 @@
+
+num = int(input("Entre com um valor: "))
+
+try:       
+    if num % 2 == 0:
+        print("Numero Par")
+    else:
+        print("Numero Impar")
+        
+except:
+    print("Não é um numero Inteiro")
+           
+ 
+     
+    
