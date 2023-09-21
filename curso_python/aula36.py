4e85d2ec59f7e852eda8921299fdde4233e70ff0
Evando Silva <evando_5@hotmail.com>
2023-09-21 05:09:14 -0300

    att

---
+++ curso_python/aula36.py
@@ -0,0 +1,11 @@
+
+condicao = True
+
+while condicao:
+    nome = input("Qual seu nome: ")
+    print(f'Seu nome é {nome}')
+    
+    if nome == 'sair':
+        break 
+print("Fim do sistema")    
+
