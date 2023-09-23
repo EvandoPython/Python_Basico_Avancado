899ced19ad05dafb01f7f978a4b78e4f947eb4b0
Evando Silva <evando_5@hotmail.com>
2023-09-23 16:07:01 -0300

    att

---
+++ curso_python/aula39.py
@@ -0,0 +1,7 @@
+
+nome = 'Evando Silva'
+
+i = 0
+while i < len(nome):
+    print(nome[i])
+    i +=1    
---
+++ curso_python/aula40.py
@@ -0,0 +1,4 @@
+while True:
+    sair = input("Quer sair [s]im: ").lower().startswith('s')
+    
+    print(sair)
