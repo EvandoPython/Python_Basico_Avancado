5b6fed7c0fc0a8f8b612cc179129b611cfa3dc90
Evando Silva <evando_5@hotmail.com>
2023-09-26 21:54:26 -0300

    att

--- curso_python/aula40.py
+++ curso_python/aula40.py
@@ -1,4 +1,39 @@
 while True:
+    n1 = input('Digite um numero: ')
+    n2 = input('Digite outro numero: ')
+    operador = input('Digite o Operador: ')
+    
+    numeros_validos = None    
+    
+    try:
+       num1 = float(n1)
+       num2 = float(n2)
+       numeros_validos = True  
+    except:
+       numeros_validos = None
+       
+       if numeros_validos is None:
+           print('Um dos numeros digitado são inválidos')   
+           continue    
+    operador_permitidos = '+-/*'
+    
+    if operador not in operador_permitidos:
+        print('Operador Invalido')
+        continue
+    
+    
+    if operador == '+':
+        print(num1 + num2)
+    elif operador == '-':
+        print(num1 - num2)    
+    elif operador == '*':
+        print(num1 * num2)        
+    elif operador == '/':
+        print(num1 / num2)        
+    
     sair = input("Quer sair [s]im: ").lower().startswith('s')
     
-    print(sair)
+    if sair is True:
+        break
+    
+print(sair)
