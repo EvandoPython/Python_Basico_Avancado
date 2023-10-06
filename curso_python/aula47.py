14c0c10e6158efed750142939d9e3242aaa79bf6
Evando Silva <evando_5@hotmail.com>
2023-10-06 12:51:42 -0300

    att

---
+++ curso_python/aula47.py
@@ -0,0 +1,9 @@
+lista = [1,2,3,4,7]
+lista [2] = 4
+del lista[2]
+
+lista.append(50)#adiciona no final da lista
+lista.append(60)
+v =lista.pop()# remover o ultimo item da lista
+print(lista)
+print(lista[2],v)
---
+++ curso_python/aula49.py
@@ -0,0 +1,9 @@
+lista = ['luz','lena','Evando','ze']
+
+indices = range(len(lista))
+
+
+
+for indice in indices:
+    print(indice, lista[indice])
+
