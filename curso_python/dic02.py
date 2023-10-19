9c002175d47f4f84d207eee746554e2e3a21dcdd
Evando Silva <evando_5@hotmail.com>
2023-10-19 12:50:55 -0300

    att

--- curso_python/dic02.py
+++ curso_python/dic02.py
@@ -5,9 +5,11 @@ perguntas = [
     'Resposta': '4',
     
     },
+    {
     'Pergunta': 'Quanto é 5*5',
     'Opções': ['25','55','10','51'],
     'Resposta': '25',
+    },
     {
     'Pergunta': 'Quanto é 10/2?',
     'Opções': ['4','5','2','1'],
@@ -15,13 +17,54 @@ perguntas = [
         
     },
     
-    
-    
-    
-    
-    
-    
 ]
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta',pergunta['Pergunta'])
+    print()
+    
+    opcoes = pergunta['Opções']
+    for i, opcao in enumerate(pergunta['Opções']):
+        print(f'{i})',opcao)
+    print()
+    
+    escolha = input('Escolha uma opção: ')
+    
+    acertou =False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+        
+    
+    if escolha_int is not None:
+        if escolha_int >=0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+       
+    print()            
+    if acertou:
+        qtd_acertos += 1
+        print('acertou ')     
+    else:
+        print("Errou")
+                   
+    
+    
+    print('Voce acertou', qtd_acertos)
+    print('de', len(perguntas),'perguntas')
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
 
        
---
+++ curso_python/set01.py
@@ -0,0 +1,2 @@
+s1 = set('Evando')
+print(s1)
