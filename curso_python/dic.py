640cb6db13841d2a2d6e4a5135084e5cbaa363bd
Evando Silva <evando_5@hotmail.com>
2023-10-23 12:47:20 -0300

    att

---
+++ curso_python/dic.py
@@ -0,0 +1,17 @@
+produto = {
+  'nome': 'Caneta Azul',
+  'preco': 2.5,
+  'categoria': 'Esxritorio',          
+          
+}
+ 
+    
+dc = {
+    chave: valor
+    for chave, valor in produto.items()
+    
+}
+
+print(dc)    
+
+        
